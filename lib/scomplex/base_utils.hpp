5cf93619eed0241676051ff104e3846e3d5561cb
crvs <carvas.f@gmail.com>
2017-01-26 20:16:11 +0100

    ditto

---
+++ lib/scomplex/base_utils.hpp
@@ -0,0 +1,47 @@
+#ifndef BASE_UTILS
+#define BASE_UTILS
+
+#include <vector>
+#include <list>
+#include <initializer_list>
+
+std::list<int> dedupe_list(std::list<int>& list) {
+    // O(n log(n))
+    std::set<int> no_reps;
+    std::list<int> no_reps_list;
+    for (int el : list) {
+        no_reps.insert(el);
+    }
+    for (int el : no_reps) {
+        no_reps_list.push_back(el);
+    }
+    return no_reps_list;
+}
+
+std::list<int> dedupe_list(std::vector<int> list) {
+    // O(n log(n))
+    std::set<int> no_reps;
+    std::list<int> no_reps_list;
+    for (int el : list) {
+        no_reps.insert(el);
+    }
+    for (int el : no_reps) {
+        no_reps_list.push_back(el);
+    }
+    return no_reps_list;
+}
+
+std::list<int> dedupe_list(std::initializer_list<int> list) {
+    // O(n log(n))
+    std::set<int> no_reps;
+    std::list<int> no_reps_list;
+    for (int el : list) {
+        no_reps.insert(el);
+    }
+    for (int el : no_reps) {
+        no_reps_list.push_back(el);
+    }
+    return no_reps_list;
+}
+
+#endif
