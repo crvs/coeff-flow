54b9fdadde9b2a80411ec15db92b690edc97d197
crvs <carvas.f@gmail.com>
2017-02-01 17:45:25 +0100

    added facilities to find nearest neighbor

--- lib/scomplex/nn_utils.hpp
+++ lib/scomplex/nn_utils.hpp
@@ -3,42 +3,44 @@
 #include <vector>
 #include <algorithm>
 
-#include "scomplex/Simplicial_Complex.h"
-#include "scomplex/graph_utils.hpp"
+#include <CGAL/Kd_tree.h>
 
-// rtree stuff
-#include "boost/geometry/index/rtree.hpp"
-#include "boost/geometry/geometries/point.hpp"
-#include "boost/geometry/geometries/box.hpp"
-
-//#include "boost/geometry/index/parameters.hpp"
-//#include "boost/geometry.hpp"
-
-// using namespace boost;
+// #include <CGAL/Simple_cartesian.h>
+#include <CGAL/Search_traits_d.h>
+#include <CGAL/Cartesian_d.h>
+#include <CGAL/K_neighbor_search.h>
+// #include <CGAL/Euclidean_distance.h>
+#include <CGAL/Orthogonal_k_neighbor_search.h>
 
 typedef std::vector<double> point_t;
 
-namespace bg = boost::geometry;
-namespace bgi = boost::geometry::index;
+typedef CGAL::Cartesian_d<double> cart;
+typedef cart::Point_d point_d;
+typedef CGAL::Search_traits_d<cart> traits;
+typedef CGAL::Orthogonal_k_neighbor_search<traits> neighbor_search_t;
+typedef neighbor_search_t::Tree tree_t;
 
-template <size_t Dimension>
-using point_template = bg::model::point<double, Dimension, bg::cs::cartesian>;
+// typedef CGAL::Kd_tree<traits> kd_tree;
+// typedef CGAL::K_neighbor_search<traits> k_neighbor;
+// typedef CGAL::Euclidean_distance<traits> euclidean;
 
-template <size_t Dimension>
-void get_r_tree_from_points<Dimension>(std::list<point_t>& points) {
-    typedef point_template<Dimension> point;
-    typedef bg::model::box<point> box;
-    typedef std::pair<box, unsigned> value;
+// template <int Dimension>
+// typedef cart::Point_d<Dimension> point_d;
 
-    // default rtree constructor
-    bgi::rtree<value, bgi::quadratic<16>> r_tree;
+void make_tree(tree_t& tree, std::list<point_t>& point_list) {
+    tree.reserve(point_list.size());
+    std::list<point_d> insertable_points;
+    for (auto pt : point_list) {
+        point_d pt_d(pt.size(), pt.begin(), pt.end());
+        tree.insert(pt_d);
+    }
+    //    return tree;
+};
 
-    /*
-        point_t,                  //
-        bgi::quadratic,           //
-        bgi::indexable<point_t>,  //
-        bgi::equal_to<point_t>,   //
-        std::allocator<point_t>   //
-        > RTree(points.begin(), points.end());
-        */
+point_t nearest_neighbour(tree_t& tree, point_t point) {
+    point_d pt(point.size(), point.begin(), point.end());
+    neighbor_search_t search(tree, pt, 1);
+    point_d result_pt = search.begin()->first;
+    point_t point_vec(result_pt.cartesian_begin(), result_pt.cartesian_end());
+    return point_vec;
 }
