d231d4b6c85c5b0f71bbceca5b50cbb68b7b039f
crvs <carvas.f@gmail.com>
2017-07-12 14:20:28 +0200

    filling in th e initializer

--- src/quotient.cpp
+++ src/quotient.cpp
@@ -1,23 +1,107 @@
 #pragma once
 
-#include <scomplex/types.hpp>
 #include <scomplex/quotient.hpp>
 #include <scomplex/simplicial_complex.hpp>
+#include <scomplex/types.hpp>
 
 /**
- * 
+ *
  **/
 
+using namespace std;
+
 struct quotient::impl {
+
+  vector<size_t> point_map;
+  vector<size_t> point_reverse_map;
+  shared_ptr<simplicial_complex> base_comp, quot_comp;
+  point_t base_point;
+
+  impl(shared_ptr<simplicial_complex> s_comp, bool(f)(point_t)) {
+    /*
+     * translate point indices
+     */
+
+    size_t i = 1;
+    size_t j = 1;
+    for (auto pt : s_comp->points) {
+      if (f(pt)) {
+        point_map.push_back(0);
+      } else {
+        point_map.push_back(i++);
+        point_reverse_map.push_back(j)
+      }
+      j++;
+    }
+
+    /*
+     * translate faces
+     */
+
+    i = 1;
+    vector<cell_t> faces;
+    while (s_comp->get_level_size(i) > 0) {
+      vector<cell_t> level;
+      faces.reserve(level.size());
+      faces.insert(faces.end(), level.begin(), level.end());
+    }
+
+    vector<cell_t> q_faces;
+    for (auto face : faces) {
+      q_faces.push_back(to_q_face(face));
+    }
+
+    this->base_comp = s_comp;
+    this->quot_comp =
+        shared_ptr<simplicial_complex>(new simplicial_complex(q_faces));
+  }
+
+  impl(const simplicial_complex &s_comp, bool(f)(point_t), point_t b_point) {
+    impl(s_comp, f);
+    this->base_point = b_point;
+  }
+
+  // TODO implement and refactor above
+  cell_t to_q_face(cell_t face) {
+    cell_t q_face;
+    bool zero = false;
+    for (auto v : face) {
+      size_t qv = point_map[v] if (qv == 0) zero = true;
+      else q_face.push_back(qv);
+    }
+    if (zero)
+      q_face.insert(q_face.begin(), 0);
+    return q_face;
+  }
+
+  cell_t to_b_face(cell_t q_face) {}
+  // to_q_point_i
+  // to_b_point_i
 };
 
+quotient::quotient(shared_ptr<simplicial_complex> s_comp, bool(quot)(point_t),
+                   point_t pt) {
+  unique_ptr<impl> p_impl(new impl(s_comp, quot, pt));
+}
+
+quotient::quotient(shared_ptr<simplicial_complex> s_comp, bool(quot)(point_t)) {
+  unique_ptr<impl> p_impl(new impl(s_comp, quot));
+}
+
+quotient::quotient(const simplicial_complex &s_comp, bool(quot)(point_t),
+                   point_t pt) {
+  unique_ptr<impl> p_impl(new impl(s_comp, quot, pt));
+}
+
+quotient::quotient(const simplicial_complex &s_comp, bool(quot)(point_t)) {
+  unique_ptr<impl> p_impl(new impl(s_comp, quot));
+}
+
+quotient::~quotient(){};
 /* TODO
  * fill in details
-quotient::quotient(const simplicial_complex&, bool(point_t));
-quotient::quotient(const simplicial_complex&, bool(point_t), point_t);
 quotient::quotient(const quotient&);
 quotient::quotient& operator=(const quotient&);
-quotient::~quotient();
 quotient::std::shared_ptr<simplicial_complex> base_complex;
 quotient::std::shared_ptr<simplicial_complex> quotient_complex;
 quotient::chain_t quotient_chain(chain_t);
