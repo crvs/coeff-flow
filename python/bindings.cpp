72511e0bc491bbb68a9dba965ff17d3275592fca
crvs <jfpbdc@kth.se>
2018-06-13 16:38:27 +0200

    retrieved diffs from python

---
+++ python/bindings.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "scomplex/coeff_flow.hpp"
+#include "scomplex/simplicial_complex.hpp"
+#include "scomplex/types.hpp"
+
+using namespace gsimp;
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(coeffflow, m) {
+    py::class_< simplicial_complex >(m, "simplicial_complex")  //
+        .def(py::init< std::vector< cell_t >& >())             //
+        .def("cell_to_index", &simplicial_complex::cell_to_index);
+
+    m.def("coeff_flow", coeff_flow);
+}
